62288c00f403d6b0e68a1efdce2ae8a49675108a
chatterjeearijit080@gmail.com <chatterjeearijit080@gmail.com>
2025-05-20 02:29:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Sidebar from "./components/Sidebar"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { useState } from "react"
+function App() {
+     const [sidebarOpen, setSidebarOpen] = useState(false)
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen)
+  }
+ return (
+  <div className="app-container">
+  <Sidebar  isOpen={sidebarOpen} toggleSidebar={toggleSidebar} ></Sidebar>
+  <div className="content">
+  <Header toggleSidebar={toggleSidebar}></Header>
+  <Footer></Footer>
+  </div>
+  </div>
+ )
+
+  
+   
+  
+}
+
+export default App
